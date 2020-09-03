a388da8b98c829d6f823eeded5f0ba7a8564bdc3
Wojciech Janoszek <wjanoszek@icloud.com>
2020-09-03 19:38:08 +0200

    Refactor usp script ┬─┬ノ( º _ ºノ)

--- assets/js/usp.js
+++ assets/js/usp.js
@@ -1,51 +1,36 @@
 ---
 ---
+const previews = [];
+const texts = [];
 const uspButtons = Array.from(document.getElementsByClassName('usp__button'));
-const uspText = document.getElementById('usp__text');
 
-const icons = ['salary.png', 'skills-big.png', 'offers-preview.png', 'career.png', 'recrutation.png'];
+class DynamicElement {
+  constructor(baseClassName, updateContentFn) {
+    this.hiddenClassName = baseClassName + '--hidden';
+    this.updateContentFn = updateContentFn;
+    this.elements = Array.from(document.getElementsByClassName(baseClassName));
+  }
 
-let texts = [];
-const texts_list = document.querySelectorAll('.usp_good__text');
-for (let text_item of texts_list) {
-  texts.push(text_item.innerText);
+  toggle(index) {
+    const hiddenEl = this.elements.find(el => el.className.includes(this.hiddenClassName));
+    if (hiddenEl) {
+      this.updateContentFn(hiddenEl, index);
+      this.elements.forEach(el => el.classList.toggle(this.hiddenClassName));
+    }
+  }
 }
 
-const togglePreview = (preview) => {
-  preview.classList.toggle('usp__preview--shown');
-  preview.classList.toggle('usp__preview--hidden');
-}
-
-const toggleText = (text) => {
-  text.classList.toggle('usp__text--shown');
-  text.classList.toggle('usp__text--hidden');
-}
+const dynamicElements = [
+  new DynamicElement('usp__text', (text, index) => text.innerText = texts[index]),
+  new DynamicElement('usp__preview', (preview, index) => preview.src = '{{ site.baseurl }}/assets/svg/' + previews[index])
+]
 
 const setButtonActive = (button, index) => {
   const activeButton = document.querySelector('.usp__button--active');
   if (activeButton) activeButton.classList.remove('usp__button--active');
   button.classList.add('usp__button--active');
 
-  if (uspText) uspText.innerText = texts[index];
-
-  const visibleImg = document.querySelector('.usp__preview--shown');
-  const hiddenImg = document.querySelector('.usp__preview--hidden');
-
-  const visibleText = document.querySelector('.usp__text--shown');
-  const hiddenText = document.querySelector('.usp__text--hidden');
-
-  if (visibleText && hiddenText) {
-    hiddenText.innerText = texts[index];
-    toggleText(visibleText);
-    toggleText(hiddenText);
-  }
-
-  if (hiddenImg && visibleImg) {
-    hiddenImg.src = '{{ site.baseurl }}/assets/svg/' + icons[index];
-    togglePreview(visibleImg);
-    togglePreview(hiddenImg);
-  }
-
+  dynamicElements.forEach(el => el.toggle(index));
   toggleAccordion(button.parentElement.querySelector('.usp__content').id);
 }
 
@@ -59,7 +44,7 @@ window.onload = () => {
   if (accordions) {
     originalHeights = accordions.map(accordion => ({ id: accordion.id, accordionHeight: accordion.offsetHeight }));
     accordions.forEach((accordion, index) => {
-      if (index > 0) accordion.style.height = '0';
+      if (index) accordion.style.height = '0';
     });
   }
 }
@@ -68,12 +53,13 @@ const toggleAccordion = (id) => {
   const accordion = document.getElementById(id);
   const accordionData = originalHeights.find(el => el.id === id);
   accordions.forEach(el => {
-    if (el.id !== id && el.offsetHeight > 0) {
+    if (el.id !== id && el.offsetHeight) {
       el.style.height = '0';
     }
   })
+
   if (accordion && accordionData) {
-    if (accordion.offsetHeight === 0) accordion.style.height = accordionData.accordionHeight + 'px';
+    if (!accordion.offsetHeight) accordion.style.height = accordionData.accordionHeight + 'px';
     else accordion.style.height = '0';
   }
 }
